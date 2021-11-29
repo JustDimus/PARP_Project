8732065888b16979e6ba50d9206dca433d0202c2
Dmytro Yevtushenko <dyevtushenko@lenovo.com>
2021-11-30 00:08:45 +0200

    Added parallel calculation

--- PARP_Project/Program.cs
+++ PARP_Project/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,42 @@ using System.Threading.Tasks;
 
 namespace PARP_Project
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var intArray = Enumerable.Range(0, 100000);
+
+            DateTime start = DateTime.Now;
+            foreach (var i in intArray)
+            {
+                Operate(i);
+            }
+
+            DateTime end = DateTime.Now;
+
+            Console.WriteLine($"Time duration (no parallel) (in ms): {(end - start).TotalMilliseconds}");
+
+
+            start = DateTime.Now;
+            Parallel.ForEach(intArray, Operate);
+            end = DateTime.Now;
+
+
+            Console.WriteLine($"Time duration (with parallel) (in ms): {(end - start).TotalMilliseconds}");
+
+            Console.ReadLine();
+        }
+
+
+        private static void Operate(int number)
+        {
+            int result = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                result += i;
+            }
         }
     }
 }
